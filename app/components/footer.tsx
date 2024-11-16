3c3cee146b9ba70da533824ca326557311b96fc2
Sam Crane <samovarfuture@gmail.com>
2024-11-16 14:47:58 -0500

    Updating social links

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,14 +24,9 @@ function SocialLink({ href, icon: Icon }) {
 function SocialLinks() {
   return (
     <div className="flex text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
-      <SocialLink href={socialLinks.twitter} icon={FaXTwitter} />
       <SocialLink href={socialLinks.github} icon={FaGithub} />
-      <SocialLink href={socialLinks.instagram} icon={FaInstagram} />
       <SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />
       <SocialLink href={socialLinks.email} icon={TbMailFilled} />
-      <a href="/rss.xml" target="_self">
-        <FaRss />
-      </a>
     </div>
   );
 }
@@ -42,7 +37,7 @@ export default function Footer() {
       <time>© {YEAR}</time>{" "}
       <a
         className="no-underline"
-        href={socialLinks.twitter}
+        href={socialLinks.linkedin}
         target="_blank"
         rel="noopener noreferrer"
       >
--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,14 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
-  ogImage: "/opengraph-image.png",
+  baseUrl: "https://scrane-portfolio.vercel.app/",
+  title: "Sam Crane | Portfolio",
+  name: "Sam",
+  ogImage: "/img/White-Crane-Logo.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  github: "https://github.com/samofuture",
+  linkedin: "https://www.linkedin.com/in/scrane11",
+  email: "scraneco@gmail.com",
 };
