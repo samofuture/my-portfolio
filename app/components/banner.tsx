a9c6132ff2c945fab514248883cbdf9fd13a575b
Sam Crane <samovarfuture@gmail.com>
2024-11-16 14:47:05 -0500

    Adding profile picture

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import ProfileImg from "../../public/img/profileImg.jpg";
+import { socialLinks } from "app/config";
 
 export default function Banner() {
     return <div className="wrapper flex flex-col md:flex-row items-center gap-10">
         <div className="w-44 h-64 rounded-full">
-            <Image 
-                className="w-full h-full rounded-full object-cover" 
-                src={ProfileImg} 
-                alt="Profile Photo"
-                priority
-            />
+            <a href={socialLinks.linkedin} target="_blank">
+                <Image 
+                    className="w-full h-full rounded-full object-cover" 
+                    src={ProfileImg} 
+                    alt="Profile Photo"
+                    priority
+                />
+            </a>
+            
         </div>
         <div className="flex flex-col items-center md:items-start">
             <h1 className="text-5xl font-bold text-white">Sam Crane</h1>
