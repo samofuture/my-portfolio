6860d81eeb118e0550b9efcc5df65ab0fd537b11
Sam Crane <samovarfuture@gmail.com>
2024-11-17 13:33:57 -0500

    Removing Photos tab and experiences section

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const navItems = {
   "/experience": {name: "Experience"},
   "/projects": { name: "Projects" },
-  "/photos": { name: "Photos" },
+  // "/photos": { name: "Photos" },
 };
 
 export function Navbar() {
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Banner from "./components/banner"
 import Aboutme from "./components/about-me";
-import Experience from "./components/experience";
 
 export default function Page() {
   return (
     <section>
       <Banner />
       <Aboutme />
-      <Experience />
     </section>
   );
 }
