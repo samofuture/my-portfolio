2d18b9fedf3e10b36a695b3e4fd7c553f1c3d52d
samofuture <54423694+samofuture@users.noreply.github.com>
2024-11-18 22:24:54 -0500

    Adding skills and spacing to projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,28 +13,38 @@ export default function Projects() {
       <h1 className="mb-8 text-4xl font-medium tracking-tight">Projects</h1>
       <div className="space-y-6">
         {projects.map((project, index) => (
-          <a
-            key={index}
-            href={project.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block group hover:opacity-80 transition-opacity duration-200"
-            id={project.title}
-          >
-            <div className="flex flex-col">
-              <div className="w-full flex justify-between items-baseline text-2xl">
-                <span className="text-black dark:text-white font-medium tracking-tight">
-                  {project.title}
-                </span>
-                <span className="text-neutral-600 dark:text-neutral-400 tabular-nums ">
-                  {project.year}
-                </span>
+          <div className="wrapper pb-8 pt-0">
+            <a
+              key={index}
+              href={project.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block group hover:opacity-80 transition-opacity duration-200"
+              id={project.title}
+            >
+              <div className="flex flex-col">
+                <div className="w-full flex justify-between items-baseline text-2xl">
+                  <span className="text-black dark:text-white font-medium tracking-tight">
+                    {project.title}
+                  </span>
+                  <span className="text-neutral-600 dark:text-neutral-400 tabular-nums ">
+                    {project.year}
+                  </span>
+                </div>
+                <p className="max-w-[90%] prose prose-neutral dark:prose-invert pt-3">
+                  {project.description}
+                </p>
               </div>
-              <p className="max-w-[90%] prose prose-neutral dark:prose-invert pt-3">
-                {project.description}
-              </p>
+            </a>
+            <div className="flex flex-wrap gap-4 max-w-full items-center pt-3">
+                {project.skills.map((skill, skillIndex) => (
+                    <button className="px-3 py-1 rounded-full transition-all duration-500 bg-gradient-to-tl to-#121212/100 via-blue-700/50 from-orange-700 bg-size-200 bg-pos-0 hover:bg-pos-100">
+                        { skill }
+                    </button>
+                ))}
+                
             </div>
-          </a>
+          </div>
         ))}
       </div>
     </section>
