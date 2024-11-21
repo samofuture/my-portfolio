37f1d71bde358b0250c32c9f339cdca0393641d4
samofuture <54423694+samofuture@users.noreply.github.com>
2024-11-20 22:41:18 -0500

    Fixing import error

--- app/components/education.tsx
+++ app/components/education.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Title from "./title";
-import Image from "next/Image";
+import Image from "next/image";
 
 export default function Education() {
     return <div className="wrapper">
